4292ba5852c912aaf7aa68ebf27e9f82097d51a2
CharlesNorsworthy <charles.norsworthy@selu.edu>
2023-11-02 13:31:10 -0500

    connecting to MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // References:
 /// https://github.com/ckorpio/classEx1/blob/main/exercise1.js
+/// https://github.com/ckorpio/MongoRender/blob/main/testmongo.js
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const fs = require("fs");
+const { MongoClient } = require("mongodb");
+const mongoUri = 'mongodb+srv://' + process.env.USERNAME + ':' + process.env.PASSWORD +
+    '@cluster0.jt7yc3y.mongodb.net/?retryWrites=true&w=majority';
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
@@ -21,6 +25,20 @@ app.get('/', function(req, res) {
         }
         res.send(data);
     })
+});
+
+app.post('/login', async function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let user = await getUser(username, password);
+    if (user === null) {
+        // TODO: say username doesn't exist or password is incorrect
+        res.send('no');
+    } else {
+        // TODO: redirect to user's personal account, send cookie
+        res.send('yes');
+    }
 });
 
 app.get('/style.css', function(req, res) {
@@ -42,3 +60,59 @@ app.get('/signup', function(req, res) {
         res.send(data);
     })
 });
+
+app.post('/signup', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const password2 = req.body.password2;
+
+    if(password !== password2) {
+        // TODO: send back html with fields filled in
+    }
+
+    // TODO: redirect to user's personal account
+
+    res.send({
+
+    });
+});
+
+app.get('/account', function(req, res) {
+
+    // TODO: see if the user is logged in via cookies, then get the account info
+
+    fs.readFile('html/account.html', 'utf8', (err, data)=> {
+        console.log('Reading html/account.html')
+        if(err) {
+            res.send('An error occurred: ' + err);
+        }
+        res.send(data);
+    })
+});
+
+async function getUser(username, password) {
+    // https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb-how-to-get-connected-to-your-database
+    const client = new MongoClient(mongoUri);
+    let returnUser = null;
+    try {
+        const database = client.db('isat_twitter');
+        const parts = database.collection('users');
+        const query = { 'username': username };
+
+        // see if this username + password combination exists
+        const user = await parts.findOne(query);
+        if(user !== null) {
+            // the user exists
+            let correctPass = user.password;
+            if(password === correctPass) {
+                returnUser = user;
+            }
+        }
+    } catch(error) {
+        console.log(error);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+    return returnUser;
+}
