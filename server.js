25e637d4a25dd17da1f0fccaf0678f3a1783220e
CharlesNorsworthy <charles.norsworthy@selu.edu>
2023-11-16 16:46:41 -0600

    the user can subscribe to topics when creating account

--- server.js
+++ server.js
@@ -24,14 +24,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set('view engine', 'ejs');
 
+const databaseName = 'isat_twitter';
+
 app.get('/style.css', function(req, res) {
     res.send(readFileAsync(__dirname + '/views/style.css'));
 });
 
 app.post('/subscribe', async function(req, res) {
-    let reqBody = req.body
+    let reqBody = req.body;
     const username = reqBody.username;
-    const subscriptions = reqBody.subscriptions
+    let subscriptions = [];
+    // include subscriptions
+    // https://stackoverflow.com/questions/17385009/can-i-iterate-over-the-query-string-parameters-using-expressjs
+    for(let queryKey in reqBody) {
+        if(reqBody.hasOwnProperty(queryKey)) {
+            let value = reqBody[queryKey].toString();
+            if(queryKey === value) {
+                subscriptions.push(queryKey);
+            }
+        }
+    }
     let user = await createUser(username, subscriptions);
     if(user !== null) {
         user.expires = new Date(Date.now() + 1000000);
@@ -60,7 +72,7 @@ app.get('/expire_cookie', function(req, res) {
     let hasCookie = cookies.hasOwnProperty('userData');
     if(hasCookie) {
         let userCookie = cookies.userData;
-        userCookie.expires = expiredTime
+        userCookie.expires = expiredTime;
         res.cookie('userData', userCookie);
         res.status(200).send('\'userData\' cookie expired.');
     } else {
@@ -76,7 +88,9 @@ app.get('*', async function (req, res) {
         let userCookie = cookies.userData;
         const expirationDate = userCookie.expires;
         if(expirationDate === undefined) {
-            res.status(200).render('subscribe', { instructions: '' });
+            let topics = await getAllTopics();
+            let data = { instructions: '', 'topics': topics };
+            res.status(200).render('subscribe', data);
         } else if(now > expirationDate) {
             res.status(419).render('login', { instructions: 'Cookie expired. Please login.' });
         } else {
@@ -85,7 +99,9 @@ app.get('*', async function (req, res) {
             let user = await getUser(username);
             if(user !== null) {
                 if(path === '/subscribe' || path === '/subscribe.html') {
-                    res.status(200).render('subscribe', { instructions: '' });
+                    let topics = await getAllTopics();
+                    let data = { instructions: '', 'topics': topics };
+                    res.status(200).render('subscribe', data);
                 } else if(path === '/login' || path === '/login.html') {
                     res.status(200).render('login', { instructions: '' });
                 } else {
@@ -94,12 +110,14 @@ app.get('*', async function (req, res) {
                     res.status(200).render('topics', user);
                 }
             } else {
-                res.status(200).render('subscribe', { instructions: '' });
+                let topics = await getAllTopics();
+                let data = { instructions: '', 'topics': topics };
+                res.status(200).render('subscribe', data);
             }
         }
     } else {
         let topics = await getAllTopics();
-        let data = { instructions: '' , 'topics': topics }
+        let data = { instructions: '', 'topics': topics };
         res.status(200).render('subscribe', data);
     }
 });
@@ -131,7 +149,7 @@ async function getAllTopics() {
     const client = new MongoClient(mongoUri);
     let topics = null;
     try {
-        const database = client.db('isat_twitter');
+        const database = client.db(databaseName);
         const collection = database.collection('topics');
         // https://stackoverflow.com/questions/33425565/how-to-return-array-of-string-with-mongodb-aggregation
         topics = await collection.distinct('topic');
@@ -146,26 +164,30 @@ async function getAllTopics() {
 async function createUser(username, subscriptions) {
     // https://www.mongodb.com/languages/mongodb-with-nodejs
     const client = new MongoClient(mongoUri);
-    let newUser = {username: username, subscriptions: subscriptions};
+    // remove subscriptions to topics that don't exist
+    let allTopics = await getAllTopics();
+    let editedSubscriptions = [];
+    for(let i in subscriptions) {
+        let s = subscriptions[i];
+        if(allTopics.includes(s)) {
+            editedSubscriptions.push(s);
+        }
+    }
+    let newUser = { username: username, subscriptions: editedSubscriptions };
     try {
-        // https://www.w3schools.com/nodejs/nodejs_mongodb_find.asp
-        const database = client.db('isat_twitter');
-        const collection = database.collection('users');
-        let query = { 'username': username };
         // see if this username exists
-        const user = await collection.findOne(query);
+        const user = await getDatabaseItem(client, 'users', { 'username': username });
         if(user === null) {
             // the user does not exist
-            await collection.insertOne(newUser);
+            await addDatabaseItem(client, 'users', newUser);
         } else {
             for(let i = 0; i <= 9999999; i++) {
                 let newUsername = username + i.toString();
-                let query = { 'username': newUsername };
-                const find = await collection.findOne(query);
+                const find = await getDatabaseItem(client, 'users', { 'username': newUsername });
                 if(find === null) {
                     // the username is unique
-                    newUser = {username: newUsername, subscriptions: subscriptions};
-                    await collection.insertOne(newUser);
+                    newUser = { username: newUsername, subscriptions: subscriptions };
+                    await addDatabaseItem(client, 'users', newUser);
                     break;
                 }
             }
@@ -177,6 +199,19 @@ async function createUser(username, subscriptions) {
         await client.close();
     }
     return newUser;
+}
+
+async function getDatabaseItem(client, collection, query) {
+    // https://www.w3schools.com/nodejs/nodejs_mongodb_find.asp
+    const db = client.db(databaseName);
+    const dbCollection = db.collection(collection);
+    return await dbCollection.findOne(query);
+}
+
+async function addDatabaseItem(client, collection, item) {
+    const db = client.db(databaseName);
+    const dbCollection = db.collection(collection);
+    await dbCollection.insertOne(item);
 }
 
 function readFileSync(filepath) {
